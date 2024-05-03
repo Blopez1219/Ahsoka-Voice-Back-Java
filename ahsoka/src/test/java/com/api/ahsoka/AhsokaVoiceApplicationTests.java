2d75767f30baf2a155f661a6574ad568757c7809
Blopez1219 <ada_hack65@hackwomen.dev>
2024-05-03 16:28:01 -0500

    Creación de proyecto
    Creación de proyecto API de Ahsoka Voice en JAVA

---
+++ ahsoka/src/test/java/com/api/ahsoka/AhsokaVoiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.ahsoka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AhsokaVoiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
