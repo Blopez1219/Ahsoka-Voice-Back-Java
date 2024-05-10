d538c3544bd41abeef3bd183ed75bcc6244199e6
Blopez1219 <ada_hack65@hackwomen.dev>
2024-05-09 22:14:12 -0500

    Login Ahsoka Voice
    Login en AhsokaVoice implementando JWT Token

---
+++ ahsoka/src/main/java/com/api/ahsoka/models/UserEntity.java
@@ -0,0 +1,48 @@
+package com.api.ahsoka.models;
+
+import java.sql.Date;
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "usuario")
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long iduser;
+
+    @Email
+    @NotBlank
+    @Size(max = 80)
+    private String email;
+
+    @NotNull(message = "La fecha de nacimiento no puede ser nula")
+    private LocalDate birthDate;
+
+    @NotBlank
+    @Size(max = 30)
+    private String username;
+
+    @NotBlank
+    private String password;
+
+    private String image;
+}
---
+++ ahsoka/src/main/java/com/api/ahsoka/services/UserDetailServiceImpl.java
@@ -0,0 +1,28 @@
+package com.api.ahsoka.services;
+
+import com.api.ahsoka.models.UserEntity;
+import com.api.ahsoka.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collection;
+import java.util.Collections;
+
+@Service
+public class UserDetailServiceImpl implements UserDetailsService {
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity userEntity = userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("El usuario "+username+" no existe"));
+
+
+        return new User(userEntity.getUsername(), userEntity.getPassword(), true, true, true, true, Collections.emptyList() );
+    }
+}
