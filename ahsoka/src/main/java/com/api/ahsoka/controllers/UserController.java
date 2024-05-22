cd8265f2f2705d884b9d4dcaf55bfbd6a93f81d8
Blopez1219 <ada_hack65@hackwomen.dev>
2024-05-21 20:38:36 -0500

    Historia de Usuario: Cambiar contrasenia
    Cambiar la contrasenia del usuario

--- ahsoka/src/main/java/com/api/ahsoka/controllers/UserController.java
+++ ahsoka/src/main/java/com/api/ahsoka/controllers/UserController.java
@@ -1,9 +1,16 @@
 package com.api.ahsoka.controllers;
 
 import com.api.ahsoka.models.UserEntity;
+import com.api.ahsoka.repositories.UserRepository;
+import com.api.ahsoka.request.UpdatePasswordDTO;
 import com.api.ahsoka.services.UserDetailServiceImpl;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/users")
@@ -11,6 +18,11 @@ public class UserController {
     @Autowired
     private UserDetailServiceImpl userDetailService;
 
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private UserRepository userRepository;
 
     @GetMapping("/hello")
     public String hello() {
@@ -20,5 +32,22 @@ public class UserController {
     @PutMapping("/updateUsername/{id}")
     public UserEntity updateUsername(@PathVariable Long id, @RequestParam String newUsername) {
         return userDetailService.updateUsername(id, newUsername);
+    }
+
+    @PutMapping("/updatePassword")
+    public ResponseEntity<?> updatePassword(@Valid @RequestBody UpdatePasswordDTO updatePasswordDTO) {
+        Optional<UserEntity> userOptional = userRepository.findById(updatePasswordDTO.getUserId());
+        if (userOptional.isPresent()) {
+            UserEntity user = userOptional.get();
+            if (passwordEncoder.matches(updatePasswordDTO.getCurrentPassword(), user.getPassword())) {
+                user.setPassword(passwordEncoder.encode(updatePasswordDTO.getNewPassword()));
+                userRepository.save(user);
+                return ResponseEntity.ok("Contraseña actualizada exitosamente");
+            } else {
+                return ResponseEntity.badRequest().body("La contraseña actual es incorrecta");
+            }
+        } else {
+            return ResponseEntity.badRequest().body("Usuario no encontrado");
+        }
     }
 }
